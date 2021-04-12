d48a01579bf23175891030a6f25a97a0f7408526
Jgammons3 <82103426+Jgammons3@users.noreply.github.com>
2021-04-12 13:05:38 -0400

    Create Jeapardy.js

---
+++ Jeapardy.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+onEvent("button4", "click", function( ) {
+  setScreen("screen2");
+  
+});
+onEvent("button1", "click", function( ) {
+  setScreen("screen3");
+});
+onEvent("button2", "click", function( ) {
+  setScreen("screen4");
+});
+onEvent("button3", "click", function( ) {
+  setScreen("screen5");
+});
+onEvent("button5", "click", function( ) {
+  setScreen("screen6");
+});
+onEvent("button6", "click", function( ) {
+  setScreen("screen7");
+});
+onEvent("button7", "click", function( ) {
+  setScreen("screen8");
+});
+
+
+onEvent("button15", "click", function( ) {
+setNumber("text_area7", + "100 ++ 100");
+setScreen("screen2");
+
