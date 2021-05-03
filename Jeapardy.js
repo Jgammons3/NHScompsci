5dddc951dae122084176959b7227848c292f96df
Jgammons3 <82103426+Jgammons3@users.noreply.github.com>
2021-05-03 14:09:05 -0400

    Update Jeapardy.js

--- Jeapardy.js
+++ Jeapardy.js
@@ -1,5 +1,5 @@
 var points = 0;
-
+var seconds = 15;
 
 
 
@@ -7,26 +7,87 @@ var points = 0;
 
 onEvent("button4", "click", function( ) {
   setScreen("screen2");
-  
+
 });
 onEvent("button1", "click", function( ) {
   setScreen("screen3");
+  timedLoop(1000, function() {
+  seconds = seconds - 1;
+  setText("text_area8", "Time: " + seconds);
+  playSound()
+if (seconds == 0) {
+   stopTimedLoop();
+   points = points - 100;
+  setText("text_area7", points);
+  setScreen("screen2");
+}
+});
 });
 onEvent("button2", "click", function( ) {
   setScreen("screen4");
+  timedLoop(1000, function() {
+  seconds = seconds - 1;
+  setText("text_area9", "Time: " + seconds);
+if (seconds == 0) {
+   stopTimedLoop();
+   points = points - 100;
+  setText("text_area7", points);
+  setScreen("screen2");
+}
+});
 });
 onEvent("button3", "click", function( ) {
   setScreen("screen5");
+  timedLoop(1000, function() {
+  seconds = seconds - 1;
+  setText("text_area10", "Time: " + seconds);
+if (seconds == 0) {
+   stopTimedLoop();
+   points = points - 100;
+  setText("text_area7", points);
+  setScreen("screen2");
+}
+});
 });
 onEvent("button5", "click", function( ) {
   setScreen("screen6");
+  timedLoop(1000, function() {
+  seconds = seconds - 1;
+  setText("text_area11", "Time: " + seconds);
+if (seconds == 0) {
+   stopTimedLoop();
+   points = points - 100;
+  setText("text_area7", points);
+  setScreen("screen2");
+}
+});
 });
 onEvent("button6", "click", function( ) {
   setScreen("screen7");
+  timedLoop(1000, function() {
+  seconds = seconds - 1;
+  setText("text_area12", "Time: " + seconds);
+if (seconds == 0) {
+   stopTimedLoop();
+   points = points - 100;
+  setText("text_area7", points);
+  setScreen("screen2");
+}
+});
 });
 onEvent("button7", "click", function( ) {
   setScreen("screen8");
+  timedLoop(1000, function() {
+  seconds = seconds - 1;
+  setText("text_area13", "Time: " + seconds);
+if (seconds == 0) {
+   stopTimedLoop();
+   points = points - 100;
+  setText("text_area7", points);
+  setScreen("screen2");
+}
 }); 
+});
 
 
 onEvent("button100(1)", "click", function( ) {
@@ -39,10 +100,16 @@ onEvent("button8","click", function( ) {
   setScreen("screen2");
 points = points - 100;
 setText("text_area7", points);
-
 });
 
 onEvent("button16","click", function( ) {
+  setScreen("screen2");
+points = points - 100;
+setText("text_area7", points);
+
+});
+
+onEvent("button14","click", function( ) {
   setScreen("screen2");
 points = points - 100;
 setText("text_area7", points);
@@ -65,7 +132,7 @@ points = points - 200;
 setText("text_area7", points);
 
 });
-onEvent("button19","click", function( ) {
+onEvent("button9","click", function( ) {
   setScreen("screen2");
 points = points - 200;
 setText("text_area7", points);
